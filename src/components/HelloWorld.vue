56f07b40b5b3cd0f8b53e42accd1666780b3bd68
sihaixianyu <sihaixianyu@gmail.com>
2023-03-09 22:29:03 +0800

    2023-03-09+

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,21 @@
 <script lang="ts">
-import { defineComponent, onBeforeMount, onMounted } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
     setup() {
         const msg = "Hello World!"
 
-        console.log("This is first console output")
-
-        onBeforeMount(() => {
-            console.log("This is from onBeforeMount function")
-        })
-
-        onMounted(() => {
-            console.log("This is from onMounted function")
-        })
-
-        console.log("This is second console output")
-
         return {
-            msg,
+            msg
         }
     }
 })
 </script>
 
-<template> m
+<template>
     <h1>
         {{ msg }}
     </h1>
 </template>
 
-<style scoped>
-.read-the-docs {
-    color: #888;
-}
-</style>
+<style scoped></style>
